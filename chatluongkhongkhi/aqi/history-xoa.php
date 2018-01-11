b923cae088803623ad3ac9e78ee468164513f512
son2408 <32300653+son2408@users.noreply.github.com>
2018-01-11 20:50:15 +0700

    Update history-xoa.php

--- chatluongkhongkhi/aqi/history-xoa.php
+++ chatluongkhongkhi/aqi/history-xoa.php
@@ -1,4 +1,14 @@
 <?php
-echo "aaaaa"
+$tentk = '';
+$tenfile ='';
+$tenfile_ngay='';
+
+
+if(isset($_GET['username'])){
+	$tentk = $_GET['username'];
+	$tenfile = $_GET['tenfile'];
+	$tenfile_ngay = $_GET['tenfile']."_ngay";
+}
+required("../../conn.php");
  
 ?>
