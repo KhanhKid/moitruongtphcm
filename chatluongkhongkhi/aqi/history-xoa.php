e39fd230d0def13e10f5e18b2f65a69c07b61107
son2408 <32300653+son2408@users.noreply.github.com>
2018-01-11 21:22:52 +0700

    Update history-xoa.php

--- chatluongkhongkhi/aqi/history-xoa.php
+++ chatluongkhongkhi/aqi/history-xoa.php
@@ -1,14 +1,14 @@
 <?php
 	
-$tentk =  $_GET['username'] ;
-$tenfile = $_GET['tenfile'];
-$tenfile_ngay = $_GET['tenfile']."_ngay";
+// $tentk =  $_GET['username'] ;
+// $tenfile = $_GET['tenfile'];
+// $tenfile_ngay = $_GET['tenfile']."_ngay";
 required("../../conn.php");
-$delete_data= " DELETE from dulieufileaqi where tentaikhoan='$tentk' and tenfile='$tenfile'";
-$drop_tbl = "DROP TABLE $tenfile";
-$drop_tbl_ngay = "DROP TABLE $tenfile_ngay";
-pg_query($conn,"$delete_data");
-pg_query($conn,"$drop_tbl");
-pg_query($conn,"$drop_tbl_ngay");
+// $delete_data= " DELETE from dulieufileaqi where tentaikhoan='$tentk' and tenfile='$tenfile'";
+// $drop_tbl = "DROP TABLE $tenfile";
+// $drop_tbl_ngay = "DROP TABLE $tenfile_ngay";
+// pg_query($conn,"$delete_data");
+// pg_query($conn,"$drop_tbl");
+// pg_query($conn,"$drop_tbl_ngay");
 
 ?>
