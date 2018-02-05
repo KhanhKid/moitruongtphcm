2c11b84c65cb8206fbe3cd09eef5722ae199b171
tandat <datca77@gmail.com>
2018-02-05 15:50:36 +0700

    uyjeryi

--- android_app/test_aqi_get_date.php
+++ android_app/test_aqi_get_date.php
@@ -4,7 +4,7 @@ include("config.inc.php");
 
 
 $thoigian= pg_query($link, "SELECT DISTINCT thoigian 
-	from aqi_hcm_thang3_2011_ngay WHERE tentram =  "."'".$_POST['station_name']."'" );
+	from aqi_hcm_2016_ngay WHERE tentram =  "."'".$_POST['station_name']."'" );
 
 $arr = pg_fetch_all($thoigian);
 	echo json_encode($arr);
