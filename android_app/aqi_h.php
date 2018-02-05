8ea853246806736a8b97db3363bde47ef46ad17d
tandat <datca77@gmail.com>
2018-02-05 15:46:15 +0700

    change data to 2016

--- android_app/aqi_h.php
+++ android_app/aqi_h.php
@@ -5,7 +5,7 @@ $thoigian = $_POST['time'];
 
 $aqi= pg_query($link, "SELECT thoigian::time, aqi_h_tram, aqi_h_so2, aqi_h_co, aqi_h_no2,
 									aqi_h_o3, aqi_h_tsp, aqi_h_pm25, aqi_h_pm10, aqi_h_pb, thongso_h_max
-							FROM aqi_hcm_thang3_2011 
+							FROM aqi_hcm_2016
 							WHERE tentram = "."'".$tentram."'".
 							" and thoigian::date = "."'".$thoigian."'"." Order by thoigian::time");
 
--- android_app/aqi_ngay_tram.php
+++ android_app/aqi_ngay_tram.php
@@ -6,7 +6,7 @@ $tentram = $_POST['station_name'];
 $thoigian = $_POST['time'];
 
 $aqi= pg_query($link, "SELECT x, y, aqi_ngay_tram, thongso_ngay_max
-						from aqi_hcm_thang3_2011_ngay
+						from aqi_hcm_2016_ngay
 						WHERE tentram = "."'".$tentram."'".
 						" and thoigian = "."'".$thoigian."'");
 $arr = pg_fetch_all($aqi);
