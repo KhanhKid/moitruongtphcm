2e3abc0cb5cf4b056380abe91fce8a19e2fedeba
tandat <datca77@gmail.com>
2018-06-05 16:39:15 +0700

    Tinh

--- Public/realtime/chart.js
+++ Public/realtime/chart.js
@@ -3,15 +3,41 @@ Highcharts.setOptions({
         useUTC: false
     }
 });
-
+var PM =[96, 84, 105, 91, 51, 85, 78, 69, 91, 102, 56, 57, 62, 70, 77, 47, 44, 100, 96, 52, 78, 101, 65, 81, 55, 86, 106, 83, 96, 71, 77, 58, 41, 75, 109, 42, 89, 103, 101, 66, 44, 82, 69, 100, 51, 46, 45, 67, 42, 89, 70, 87, 69, 92, 104, 62, 50, 107, 57, 60, 60, 87, 109, 44, 52, 71, 72, 64, 64, 72, 99, 70];
+function returnPlotLines(now){
+    var res = [];
+    for (var i = - 47; i <= 0; i ++) {
+        var dt = now + i * 60*60*1000;
+        var date = new Date(dt);
+        var hour = date.getHours();
+        if ( hour == 0) {
+            res.push({
+                time : dt,                
+                hour :hour
+            });
+        } else if(date.getHours() == 12){
+            res.push({
+                time :dt,                
+                hour : hour
+            });
+        }
+    }
+    return res;
+}
 
 
 function AddChart(id, minV, maxV){
     var pointWidth = (document.getElementById(id).offsetWidth)/48;
+    var now = (new Date()).getTime();
+    var plotLine = returnPlotLines(now);
     var dulieu =[];
-    for (var i = 0; i <= 72; i++) {
-        dulieu.push(Math.floor(Math.random()*maxV) + minV);
-    }
+    if (id == 'pm') {
+       dulieu = PM; 
+    } else{
+        for (var i = 0; i <= 72; i++) {
+            dulieu.push(Math.floor(Math.random()*maxV) + minV);
+        }
+    };    
 
     Highcharts.chart( id, {
         chart: {
@@ -19,8 +45,6 @@ function AddChart(id, minV, maxV){
             animation: Highcharts.svg, // don't animate in old IE
             events: {
                 load: function () {
-
-                    // set up the updating of the chart each second
                     var series = this.series[0];
                     // setInterval(function () {
                     //     var x = (new Date()).getTime(),// current time
@@ -30,16 +54,52 @@ function AddChart(id, minV, maxV){
                     // }, 3600000);
                 }
             },
-            marginTop:0,
+            marginTop: (id =='pm') ? 25 : 1,
             marginLeft:-1,
             marginRight:0,
-            marginBottom:0
+            marginBottom: 0
         },
         title: {
             text: ''
         },
         xAxis: {
             type: 'datetime',
+            plotLines:(function(){
+                var data =[];
+                var Day = ['Chủ nhật','Thứ hai','Thứ ba','Thứ tư','Thứ năm','Thứ sáu','Thứ bảy'];
+                for (var i=0; i < plotLine.length ; i++ ){
+                    if (plotLine[i].hour == 0) {
+                        data.push({
+                            color:'#808080',
+                            value: plotLine[i].time ,
+                            dashStyle:'ShortDash',
+                            label: {
+                                text: Day[(new Date(plotLine[i].time)).getDay()],
+                                verticalAlign:'top',
+                                align:'center',
+                                rotation:0,
+                                y: -5,
+                            },
+                            width:1,
+                        });
+                    } else{
+                        data.push({
+                            color:'green',
+                            label:{
+                                text: '12:00',
+                                verticalAlign:'top',
+                                align:'center',
+                                rotation:0,
+                                y: -5,
+                            },
+                            value: plotLine[i].time ,
+                            dashStyle:'Dot',
+                            width:1,
+                        })
+                    }
+                }
+                return data;
+            }())
         },
         yAxis: {
             title: {
@@ -133,10 +193,7 @@ function AddChart(id, minV, maxV){
         var data = chart.yAxis[0].series[0].processedYData;
         var AQI = Highcharts.numberFormat(data[data.length -1],0);
         $("#aqiValue").text(AQI);
-        var xData =  chart.series[0].xData
-        var dtime = xData[xData.length -1];
-        var time = new Date(dtime);
-        $("#currentTime").text(time.toUTCString());
+        var date =  chart.xAxis[0];
         var obj = returnValue(AQI);
         if (obj) {
             $("#trangthaiAQI").text(obj.trangThai);
@@ -183,9 +240,78 @@ function returnValue(AQI){
 
 }
 
+
+
+function addAxis(){
+    var pointWidth = (document.getElementById('axis').offsetWidth)/48;
+    var now = (new Date()).getTime();
+ 
+    Highcharts.chart( 'axis', {
+        chart: {
+            type: 'column',
+            animation: Highcharts.svg, // don't animate in old IE
+            events: {
+                load: function () {
+                    var series = this.series[0];
+                }
+            },
+            marginTop:0,
+            marginLeft:-1,
+            marginRight:0,
+            marginBottom: 40
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            type: 'datetime',
+        },
+        yAxis: {
+            title: {
+                text: ''
+            },
+            // plotLines:[
+            //     {
+            //         color:'red',
+            //         value:1,
+            //         width:2,
+            //         zIndex:4
+            //     },
+            // ]
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            data: (function () {
+                var data = [],
+                time = (new Date()).getTime(),
+                i; 
+                for (i = - 47; i <= 0; i += 1) {
+                    data.push({
+                        x: time + i * 60*60*1000,
+                        y: 0
+                    });
+                }
+                return data;
+            }())
+        }],
+        plotOptions:{
+            column:{                
+                pointPadding:0,
+                borderWidth:0.5,
+                pointWidth: pointWidth
+            }            
+        }
+    });
+}
 AddChart('pm',20,110);
 AddChart('no',50,160);
 AddChart('no2',5,110);
 AddChart('so2',70,160);
 AddChart('o3',20,100);
 AddChart('gio',30,100);
+addAxis();
--- realtime.php
+++ realtime.php
@@ -1,23 +1,29 @@
-	<div class="container-fluid" style="padding: 20px">
-		<div class="row" style="height:400px">
-			<div class="col-md-7" style="">
+<div class="container-fluid">
+		<div class=" row" style="height:400px">
+			<div class="col-md-6" style="">
 				<div style="" id='panelChart' class="panelBorder">
 					<div id="AQI">
-						<div class="col-sm-4" id='aqiValue'>
-						</div>
-						<div class="col-sm-8">
-							<div id='trangthaiAQI'>
+							<div class="col-sm-4" id='aqiValue'>
 							</div>
-							<div id='chuthichAQIAQI'>
-								<p>
-									<label>Cập nhật lúc:</label>
-									<span id="currentTime">
-									</span>
-								</p>
+							<div class="col-sm-8">
+								<div id='trangthaiAQI'>
+								</div>
+								<div id='chuthichAQIAQI'>
+									<p>
+										<label>Cập nhật lúc:</label>
+										<span>
+											Ngày 4/4/2048 15h
+										</span>
+									</p>
+								</div>
 							</div>
-						</div>
 					</div>
 					<div id= 'chart-container'>
+						<div class="row">
+							<div class="col-md-8 col-md-push-2">
+								<div id='axis'></div>
+							</div>
+						</div>
 						<div class="row">
 							<div class="col-md-2 thongso">
 								<div class="col-sm-6 tenthongso">
@@ -29,9 +35,8 @@
 								</div>
 							</div>
 							<div class="col-md-8">
-								<div  class="chart" id ="pm">
-
-								</div>
+								<div  class="chart" id ="pm" style="height: 100px"></div>
+								<!-- <div id='axis'></div> -->
 							</div>
 							<div class="col-md-2 minmax">
 								<div class="col-sm-6">
@@ -177,127 +182,109 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-5" style=" height:600px">
-				<div class="row">
-					<div class="col-md-12 panelBorder" id="map-realtime" style="height: 400px;"></div>
-					<div class="col-md-12 panelBorder" id="map-realtime" style="height: 200px"><img src="img/giatriAQI.png" style="width:100%;height:100%"></div>
-				</div>
+			<div class="col-md-6 panelBorder" style=" height:640px" id="map-realtime">
+				
 			</div>
 		</div>
-	</div>
-	<div class="row" >
-		<div class="col-md-12">
-			<div  class="panelBorder" id='panelDuBao'>
-				<div class="row head-panelDuBao">
-					<div class="col-md-8 col-md-push-2">
-						<div class="row" style="margin: auto;text-align: center;">
-							<div class="totalDuBao" id='ngayDuBao_0'>
-								<span class="glyphicon glyphicon-cloud"></span>
-								<label class="tenNgay"></label>
-								<div class="tempMin"></div>
-								<div class="tempMax"></div>
-							</div>
-							<div class="totalDuBao" id='ngayDuBao_1'>
-								<span class="glyphicon glyphicon-cloud"></span>
-								<label class="tenNgay"></label>
-								<div class="tempMin"></div>
-								<div class="tempMax"></div>
-							</div>
-							<div class="totalDuBao" id='ngayDuBao_2'>
-								<span class="glyphicon glyphicon-cloud"></span>
-								<label class="tenNgay"></label>
-								<div class="tempMin"></div>
-								<div class="tempMax"></div>
-							</div>
-							<div class="totalDuBao" id='ngayDuBao_3'>
-								<span class="glyphicon glyphicon-cloud"></span>
-								<label class="tenNgay"></label>
-								<div class="tempMin"></div>
-								<div class="tempMax"></div>
-							</div>
-							<div class="totalDuBao" id='ngayDuBao_4'>
-								<span class="glyphicon glyphicon-cloud"></span>
-								<label class="tenNgay"></label>
-								<div class="tempMin"></div>
-								<div class="tempMax"></div>
-							</div>
-							<div class="totalDuBao" id='ngayDuBao_5'>
-								<span class="glyphicon glyphicon-cloud"></span>
-								<label class="tenNgay"></label>
-								<div class="tempMin"></div>
-								<div class="tempMax"></div>
+		<div class="row" >
+			<div class="col-md-12">
+				<div  class="panelBorder" id='panelDuBao'>
+					<div class="row head-panelDuBao">
+						<div class="col-md-8 col-md-push-2">
+							<div class="row" style="margin: auto;text-align: center;">
+								<div class="totalDuBao" id='ngayDuBao_0'>
+									<span class="glyphicon glyphicon-cloud"></span>
+									<label class="tenNgay"></label>
+									<div class="tempMin"></div>
+									<div class="tempMax"></div>
+								</div>
+								<div class="totalDuBao" id='ngayDuBao_1'>
+									<span class="glyphicon glyphicon-cloud"></span>
+									<label class="tenNgay"></label>
+									<div class="tempMin"></div>
+									<div class="tempMax"></div>
+								</div>
+								<div class="totalDuBao" id='ngayDuBao_2'>
+									<span class="glyphicon glyphicon-cloud"></span>
+									<label class="tenNgay"></label>
+									<div class="tempMin"></div>
+									<div class="tempMax"></div>
+								</div>
+								<div class="totalDuBao" id='ngayDuBao_3'>
+									<span class="glyphicon glyphicon-cloud"></span>
+									<label class="tenNgay"></label>
+									<div class="tempMin"></div>
+									<div class="tempMax"></div>
+								</div>
+								<div class="totalDuBao" id='ngayDuBao_4'>
+									<span class="glyphicon glyphicon-cloud"></span>
+									<label class="tenNgay"></label>
+									<div class="tempMin"></div>
+									<div class="tempMax"></div>
+								</div>
+								<div class="totalDuBao" id='ngayDuBao_5'>
+									<span class="glyphicon glyphicon-cloud"></span>
+									<label class="tenNgay"></label>
+									<div class="tempMin"></div>
+									<div class="tempMax"></div>
+								</div>
 							</div>
 						</div>
 					</div>
+					<div class="row">
+						<div class="col-xs-1">
+							<div style="height: 50px; margin-top: 50px; font-weight: bold"> 
+								Nhiệt độ
+							</div>
+							<div style="height: 75px; margin-top:35px; font-weight: bold"> 
+								Gió
+							</div>
+							<div style="height: 75px; font-weight: bold"> 
+								Thời tiết
+							</div>
+						</div>
+						<div class="col-xs-11">
+							<div class="row">
+								<div class="col-md-2 chartdubao" id="chartDuBao_0">
+									<div class="chart-title"></div>
+									<div class="chart temp" id='temp_0'></div>
+									<div class="chart gio" id="gio_0"></div>
+									<div class="chart weather"></div>
+								</div>
+								<div class="col-md-2 chartdubao" id="chartDuBao_1">
+									<div class="chart-title"></div>
+									<div class="chart temp" id='temp_1'></div>
+									<div class="chart gio" id="gio_1"></div>
+									<div class="chart weather"></div>
+								</div>
+								<div class="col-md-2 chartdubao" id="chartDuBao_2">
+									<div class="chart-title"></div>
+									<div class="chart temp" id='temp_2'></div>
+									<div class="chart gio" id="gio_2"></div>
+									<div class="chart weather"></div>
+								</div>
+								<div class="col-md-2 chartdubao" id="chartDuBao_3">
+									<div class="chart-title"></div>
+									<div class="chart temp" id='temp_3'></div>
+									<div class="chart gio" id="gio_3"></div>
+									<div class="chart weather"></div>
+								</div>
+								<div class="col-md-2 chartdubao" id="chartDuBao_4">
+									<div class="chart-title"></div>
+									<div class="chart temp" id='temp_4'></div>
+									<div class="chart gio" id="gio_4"></div>
+									<div class="chart weather"></div>
+								</div>
+								<div class="col-md-2 chartdubao" id="chartDuBao_5"> 
+									<div class="chart-title"></div>
+									<div class="chart temp" id='temp_5'></div>
+									<div class="chart gio" id="gio_5"></div>
+									<div class="chart weather"></div>
+								</div>
+							</div>
+						</div>
+					</div>					
 				</div>
-				<div class="row">
-					<div class="col-xs-1">
-						<div style="height: 50px; margin-top: 50px"> 
-							Nhiệt độ
-						</div>
-						<div style="height: 75px; margin-top:35px"> 
-							Gió
-						</div>
-					</div>
-					<div class="col-xs-11">
-						<div class="row">
-							<div class="col-md-2 chartdubao" id="chartDuBao_0">
-								<div class="chart-title"></div>
-								<div class="chart temp" id='temp_0'>
-
-								</div>
-								<div class="chart gio" id="gio_0" style="height: 75px">
-
-								</div>
-							</div>
-							<div class="col-md-2 chartdubao" id="chartDuBao_1">
-								<div class="chart-title"></div>
-								<div class="chart temp" id='temp_1'>
-
-								</div>
-								<div class="chart gio" id="gio_1" style="height: 75px">
-
-								</div>
-							</div>
-							<div class="col-md-2 chartdubao" id="chartDuBao_2">
-								<div class="chart-title"></div>
-								<div class="chart temp" id='temp_2'>
-
-								</div>
-								<div class="chart gio" id="gio_2" style="height: 75px">
-
-								</div>
-							</div>
-							<div class="col-md-2 chartdubao" id="chartDuBao_3">
-								<div class="chart-title"></div>
-								<div class="chart temp" id='temp_3'>
-
-								</div>
-								<div class="chart gio" id="gio_3" style="height: 75px">
-
-								</div>
-							</div>
-							<div class="col-md-2 chartdubao" id="chartDuBao_4">
-								<div class="chart-title"></div>
-								<div class="chart temp" id='temp_4'>
-
-								</div>
-								<div class="chart gio" id="gio_4" style="height: 75px">
-
-								</div>
-							</div>
-							<div class="col-md-2 chartdubao" id="chartDuBao_5"> 
-								<div class="chart-title"></div>
-								<div class="chart temp" id='temp_5'>
-
-								</div>
-								<div class="chart gio" id="gio_5" style="height: 75px">
-
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>					
 			</div>
 		</div>
 	</div>
